a8906d983e38327cada207d97e0211cf32a4c7da
Kousik Satish <kousiksatish@gmail.com>
2020-05-09 00:11:46 +0530

    Add variable path and indentation

--- x264_build_script.sh
+++ x264_build_script.sh
@@ -1,52 +1,53 @@
-ANDROID_NDK=/home/kousik/Downloads/android-ndk-r14b/android-ndk-r14b
+ANDROID_NDK=<Path to NDK>/android-ndk-r14b/android-ndk-r14b
 SDK_VERSION=21
 
-build() {
-ARCH=$1
+build() 
+{
+    ARCH=$1
 
-if [ "$ARCH" = "arm64" ]; then
-    PLATFORM_PREFIX="aarch64-linux-android-"
-    HOST="aarch64-linux"
-    PLATFORM_VERSION=4.9
-elif [ "$ARCH" = "arm" ]; then
-    PLATFORM_PREFIX="arm-linux-androideabi-"
-    HOST="arm-linux"
-    PLATFORM_VERSION=4.9
-elif [ "$ARCH" = "x86" ]; then
-    PLATFORM_PREFIX="i686-linux-android-"
-    HOST="i686-linux"
-    PLATFORM_VERSION=4.9
-elif [ "$ARCH" = "x86_64" ]; then
-    PLATFORM_PREFIX="x86_64-linux-android-"
-    HOST="x86_64-linux"
-    PLATFORM_VERSION=4.9
-else
-    echo "unsupport ARCH:$ARCH."
-    exit
-fi
+    if [ "$ARCH" = "arm64" ]; then
+        PLATFORM_PREFIX="aarch64-linux-android-"
+        HOST="aarch64-linux"
+        PLATFORM_VERSION=4.9
+    elif [ "$ARCH" = "arm" ]; then
+        PLATFORM_PREFIX="arm-linux-androideabi-"
+        HOST="arm-linux"
+        PLATFORM_VERSION=4.9
+    elif [ "$ARCH" = "x86" ]; then
+        PLATFORM_PREFIX="i686-linux-android-"
+        HOST="i686-linux"
+        PLATFORM_VERSION=4.9
+    elif [ "$ARCH" = "x86_64" ]; then
+        PLATFORM_PREFIX="x86_64-linux-android-"
+        HOST="x86_64-linux"
+        PLATFORM_VERSION=4.9
+    else
+        echo "unsupport ARCH:$ARCH."
+        exit
+    fi
 
-PREFIX=~/android/${ARCH}
-SYSROOT=$ANDROID_NDK/platforms/android-${SDK_VERSION}/arch-${ARCH}
-TOOLCHAIN=$ANDROID_NDK/toolchains/${ARCH}-${PLATFORM_VERSION}/prebuilt/linux-x86_64
-CC=$TOOLCHAIN/bin/${PLATFORM_PREFIX}gcc
-CXX=$TOOLCHAIN/bin/${PLATFORM_PREFIX}c++
-CROSS_PREFIX=$TOOLCHAIN/bin/${PLATFORM_PREFIX}
-NM=$TOOLCHAIN/bin/${PLATFORM_PREFIX}nm
+    PREFIX=~/android/${ARCH}
+    SYSROOT=$ANDROID_NDK/platforms/android-${SDK_VERSION}/arch-${ARCH}
+    TOOLCHAIN=$ANDROID_NDK/toolchains/${ARCH}-${PLATFORM_VERSION}/prebuilt/linux-x86_64
+    CC=$TOOLCHAIN/bin/${PLATFORM_PREFIX}gcc
+    CXX=$TOOLCHAIN/bin/${PLATFORM_PREFIX}c++
+    CROSS_PREFIX=$TOOLCHAIN/bin/${PLATFORM_PREFIX}
+    NM=$TOOLCHAIN/bin/${PLATFORM_PREFIX}nm
 
-./configure \
-    --prefix=$PREFIX \
-    --cross-prefix=$CROSS_PREFIX \
-    --sysroot=$SYSROOT \
-    --host=$HOST \
-    --prefix=$PREFIX \
-    --enable-shared \
-    --disable-asm \
-    --enable-pic \
-    --disable-cli \
+    ./configure \
+        --prefix=$PREFIX \
+        --cross-prefix=$CROSS_PREFIX \
+        --sysroot=$SYSROOT \
+        --host=$HOST \
+        --prefix=$PREFIX \
+        --enable-shared \
+        --disable-asm \
+        --enable-pic \
+        --disable-cli \
 
-make clean
-make -j4
-make install
+    make clean
+    make -j4
+    make install
 }
 
 build arm
